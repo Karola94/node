d5d000e8f9c9f0b51c81dd2659f88a9d348671cf
Karolina Kuchta <linka_94@interia.eu>
2019-08-28 14:49:11 +0200

    Read and write files

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
-process.stdin.setEncoding('utf-8');
+var fs = require('fs');
+var colors = require('colors');
+// var StatMode = require('stat-mode');
 
-process.stdin.on('readable', function() {
-    var input = process.stdin.read();    
-    if(input !== null) {        
-        var instruction = input.toString().trim();
-        switch(instruction) {
-            case '/exit':
-                process.stdout.write('Quitting app!\n');
-                process.exit();
-                break;
-            case '/version':
-                console.log(process.versions);
-                break;
-            case '/lang':
-                console.log(process.env.LANG);
-                break;
-            default:
-                process.stderr.write('Wrong instruction!');
-        }     
-    }
-});
+// fs.stat('./cat.jpg', function(err, stats) {
+//     var statMode = new StatMode(stats);
+//     console.log(statMode.toString());
+// });
+
+fs.readdir('.', 'utf-8', function(err, data) {
+    if(err) throw err;
+    console.log('Odczyt obecnego katalogu.'.yellow);
+    fs.writeFile('./tekst.txt', data, function(err) {
+        if(err) throw err;
+        console.log('Zapisano!'.green);
+    });
+}); 
+// fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
+//     console.log('Dane przed zapisem!'.blue);
+//     console.log(data);
+//     fs.appendFile('./tekst.txt', '\nA tak wyglądają po zapisie!', function(err) {
+//         if(err) throw err;
+//         console.log('Zapisano!'.blue);
+//         fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
+//             console.log('Dane po zapisie'.blue);
+//             console.log(data);
+//         });
+//     });
+// });
