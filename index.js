02b5c67977a01986cdf177c8b60faf78a47cac58
Karolina Kuchta <linka_94@interia.eu>
2019-08-29 13:29:23 +0200

    Personal server

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
-process.stdin.setEncoding('utf-8');
+var http = require('http');
+var fs = require('fs');
 
-process.stdin.on('readable', function() {
-    var input = process.stdin.read();    
-    if(input !== null) {        
-        var instruction = input.toString().trim();
-        switch(instruction) {
-            case '/exit':
-                process.stdout.write('Quitting app!\n');
-                process.exit();
-                break;
-            case '/version':
-                console.log(process.versions);
-                break;
-            case '/lang':
-                console.log(process.env.LANG);
-                break;
-            default:
-                process.stderr.write('Wrong instruction!');
-        }     
+var server = http.createServer();
+
+// server.on('request', function(request, response) {
+//     response.write('Hello world!');
+//     response.end();
+// });
+// server.listen(9000);
+
+// fs.readFile('./index.html', 'utf-8', function(err, data) {
+//     console.log('Odczyt pliku html.');
+//     console.log(data);
+// });
+
+server.on('request', function (request, response) {
+    response.setHeader("Content-Type", "text/html; charset=utf-8");
+    if (request.method === 'GET' && request.url === '/index.html') {                          
+        fs.readFile('./index.html', 'utf-8', function(err, data) {    
+            console.log(data);
+            response.write(data);  
+            response.end();        
+        });       
+    } else {
+        fs.readFile('./index.html', 'utf-8', function(err, data) {            
+            response.write('<img src="404error.jpeg" alt="Not found"/>');
+            response.end();                   
+        });   
     }
 });
+
+server.listen(9000);
