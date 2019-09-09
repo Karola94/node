7f03e375cab2f5ebe875525503decbecf4197b81
Nemethe <pl.nemethe@gmail.com>
2019-09-09 19:35:35 +0200

    n

--- index.js
+++ index.js
@@ -15,19 +15,33 @@ var server = http.createServer();
 // });
 
 server.on('request', function (request, response) {
-    response.setHeader("Content-Type", "text/html; charset=utf-8");
-    if (request.method === 'GET' && request.url === '/index.html') {                          
-        fs.readFile('./index.html', 'utf-8', function(err, data) {    
+    if (request.method === 'GET' && request.url === '/index.html') {
+        response.setHeader("Content-Type", "text/html; charset=utf-8");
+        fs.readFile('./index.html', 'utf-8', function(err, data) {
             console.log(data);
-            response.write(data);  
-            response.end();        
-        });       
+            response.write(data);
+            response.end();
+        });
+    } else if (request.method === 'GET' && request.url === '/404error.jpeg') {
+        response.setHeader("Content-Type", "image/jpeg");
+        fs.readFile('./404error.jpeg', function(err, data) {
+            console.log(data);
+            response.write(data);
+            response.end();
+        });
+    } else if (request.method === 'GET' && request.url === '/worked.jpg') {
+        response.setHeader("Content-Type", "image/jpeg");
+        fs.readFile('./worked.jpg', function(err, data) {
+            console.log(data);
+            response.write(data);
+            response.end();
+        });
     } else {
-        fs.readFile('./index.html', 'utf-8', function(err, data) { 
-                   
+        // fs.readFile('./index.html', 'utf-8', function(err, data) {
+            response.setHeader("Content-Type", "text/html; charset=utf-8");
             response.write('<img src="404error.jpeg" alt="Not found"/>');
-            response.end();                   
-        });   
+            response.end();
+        // });
     }
 });
 
